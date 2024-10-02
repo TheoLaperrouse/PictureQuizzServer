a515af65873d8d0208b9a95a2fef4263191b9464
Théo Laperrouse <theo.laperrouse@energiency.com>
2024-10-03 01:00:26 +0200

    feat: add socker connection

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(cors({
-  origin: 'http://localhost:8080',
+  origin: 'http://localhost:8080', 
   methods: ['GET', 'POST'],
 }));
+
 
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
@@ -30,7 +31,6 @@ io.on('connection', (socket) => {
   });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
